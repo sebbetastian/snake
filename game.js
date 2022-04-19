aa43187159392693a335309acc255f852ee9a14a
Sebastian Aanstad <sebbeaanstad@gmail.com>
2022-04-19 20:14:03 +0200

    added comments

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
+//getting the touch controls
 const up = document.getElementById('button0');
 const down = document.getElementById('button1');
 const left = document.getElementById('button2');
 const right = document.getElementById('button3');
-const controls = [up, down, left, right];
-
 //defining snake
 let snake = [  
     {x: 150, y: 150},
@@ -12,22 +11,24 @@ let snake = [
     {x: 120, y: 150},  
     {x: 110, y: 150},  
 ];
-
+//important variables
 let directionX = 10;
 let directionY = 0;
 let changingDirection = false;
 let foodX;
 let foodY;
 let score = 0;
-
+//gets the html canvas element
 const gameCanvas = document.getElementById('game-canvas');
 const ctx  = gameCanvas.getContext('2d');
-
+//inits the entire game
 main();
 genFood();
 
+//adds event handlers to keyboard controls
 document.addEventListener('keydown', change_direction)
 
+//adds event handlesrs to touch controls
 up.addEventListener('click', () => {
     const goingDown = directionY === 10;
     if (!goingDown) {
@@ -59,12 +60,9 @@ right.addEventListener('click', () => {
         directionY = 0;
     }
 })
-
+//the game
 function main() {
-    if (endGame()) {
-        return alert('you lost, reload the page to try again!');
-    };
-    
+    if (endGame()) return alert('you lost, reload the page to try again!');
     changingDirection = false;
     setTimeout(function onTick() {
         clearCanvas();
@@ -72,20 +70,20 @@ function main() {
         moveSnake();
         drawSnake();
         main();
-    }, 150);
+    }, 150);//fasst ssssnakes only!
 }
-
+//resets the game
 function clearCanvas() {
     ctx.fillStyle = 'white';
     ctx.strokestyle = 'black';
     ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
     ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
-
+//drawing the actual snake
 function drawSnake () {
     snake.forEach(drawSnakeBlocks);
 }
-
+//drawing the food
 function drawFood() {
     ctx.fillStyle = 'red';
     ctx.strokestyle = 'lightcoral';
@@ -93,14 +91,14 @@ function drawFood() {
     ctx.strokeRect(foodX, foodY, 10, 10);
 }
 
-//drawing the snake
+//drawing the snake parts
 function drawSnakeBlocks(snakePart) {
     ctx.fillStyle = 'blueviolet';
     ctx.strokestyle = 'white';
     ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
-
+//ends the game
 function endGame() {
     for (let i = 4; i < snake.length; i++)  {
         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
@@ -111,10 +109,11 @@ function endGame() {
     const hitBottomWall = snake[0].y > gameCanvas.height - 10;
     return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
 }
-
+//adds a random coordinate for food
 function randomFood(min, max) {
     return Math.round((Math.random() * (max-min) + min) / 10) * 10;
 }
+//makes the food
 function genFood() {
     foodX = randomFood(0, gameCanvas.width - 10);
     foodY = randomFood(0, gameCanvas.width - 10);
@@ -123,6 +122,7 @@ function genFood() {
         if (has_eaten) genFood();
     })
 }
+//keyboard controls
 function change_direction(KeyboardEvent) {    
     if (changingDirection) return;
     changingDirection = true;
@@ -157,7 +157,7 @@ function change_direction(KeyboardEvent) {
           directionY = 10;
      }
 }
-
+//moves the snake, makes it eat AND increments the score
 function moveSnake() {
     const head = {x: snake[0].x + directionX, y: snake[0].y + directionY};
     snake.unshift(head);
